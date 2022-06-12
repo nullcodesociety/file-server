1f3783a2782fe42efd403f83da03b8d2cba8dd12
John Funk (Underscorefunk) <john@underscorefunk.com>
2022-06-12 15:23:04 -0500

    Create prelude

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
+// Create entry point in the crate for modules
+mod config;
 mod server;
 mod request;
 mod response;
-mod status_code;
+mod resource;
 
+mod prelude {
+    // Declare module components as usable in the prelude for inclusion
+    // through `use prelude::*` for convenience.
+    pub use crate::config::*;
+    pub use crate::server::*;
+    pub use crate::request::*;
+    pub use crate::response::*;
+    pub use crate::resource::*;
+
+
+}
+
+use prelude::*;
 use std::env;
 
 #[tokio::main]
 async fn main() {
 
-    match config::from_env_args(env::args()) {
+    match config::from(env::args()) {
 
         Err(e) => {
             println!("config error: {:?}", e.message);
