827242e7f12f063ad308596bc246ecd5376bd3ea
John Funk <john@underscorefunk.com>
2022-07-07 22:17:44 -0500

    Clean up response generation path

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 #![allow(dead_code)]
 pub mod config;
 pub mod server;
-pub mod response;
 pub mod resource;
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use crate::config::{Config};
+use crate::resource;
 
 use std::future::Future;
 use std::convert::Infallible;
@@ -14,6 +15,13 @@ use hyper::service::{make_service_fn, service_fn};
 
 use tokio::fs::File;
 use tokio_util::codec::{BytesCodec, FramedRead};
+
+#[derive(Debug)]
+enum ServerError {
+    ResourcePath(io::Error),
+    FileOpen(path::PathBuf),
+    Response
+}
 
 pub async fn start(config: Config) -> Result<String, String> {
 
@@ -47,100 +55,90 @@ pub async fn start(config: Config) -> Result<String, String> {
     }
 }
 
+
 async fn handle_request(
     resource_root: path::PathBuf,
     request: hyper::Request<hyper::Body>,
 ) -> Result<hyper::Response<hyper::Body>, Infallible> {
 
-    // The result has to always be ok so we're going to move the
-    // actually response generator to avoid redundant Result wrapping
-    Ok(generate_response(resource_root, request).await)
+    let request_path = path::PathBuf::from(request.uri().path());
+    println!("Request: {:?}", request_path);
+
+    Ok(generate_response(resource_root, request_path).await)
 }
+
 
 async fn generate_response(
     resource_root: path::PathBuf,
-    request: hyper::Request<hyper::Body>,
+    request_path: path::PathBuf
 ) -> Response<Body> {
 
-    let request_path = path::PathBuf::from( request.uri().path() );
-    println!("Request: {:?}", request_path);
-
-    if let Ok(requested_resource_path) = crate::resource::path(
-        resource_root.clone(),
-        request_path.clone(),
-    ) {
-        if let Ok(desired_response) = file_response(requested_resource_path).await {
-            println!("  OK  ");
-            return desired_response;
+    match file_response(resource_root.clone(), request_path).await {
+        Ok(r) => r,
+        Err(e) => match file_response(resource_root.clone(), error_path()).await {
+            Ok(r) => r,
+            Err(e) => failure_response()
         }
     }
 
-    let error_path = error_path("404");
-    if let Ok( error_resource_path )  = crate::resource::path(
+}
+
+
+async fn file_response(
+    resource_root: path::PathBuf,
+    request_path: path::PathBuf
+) -> Result<Response<Body>, ServerError> {
+
+    let resource_path = match resource::path(
         resource_root,
-        error_path
+        request_path,
     ) {
-        if let Ok(error_response) = file_response(error_resource_path).await {
-            println!("  HANDLED ERROR  ");
-            return error_response;
+        Err(e) => return Err(ServerError::ResourcePath(e)),
+        Ok(r) => r
+    };
+
+    match file_response_body(resource_path.clone()).await {
+        Err(e) => return Err(e),
+        Ok(body) => {
+            let status_code = StatusCode::OK;
+            let content_type = "TXT";
+            let response = hyper::Response::builder()
+                .status(status_code)
+                .header(CONTENT_TYPE, content_type)
+                .body(body);
+            match response {
+                Ok(body) => Ok(body),
+                Err(_) => Err(ServerError::Response)
+            }
+
         }
     }
-
-    println!("\
-        Unhandled error: \n\
-         -> Dead End",
-    );
-    server_error_response()
-}
-
-async fn file_response(requested_resource_path: path::PathBuf) -> Result<Response<Body>, Error> {
-    //@todo get the content type and body
-    //@todo inject status code properly
-    match file_response_body(requested_resource_path).await {
-        Ok(response_body) => {
-            let r = prepare_response(StatusCode::OK, "TXT", response_body).await;
-            Ok(r)
-        },
-        Err(e) => Err(e)
-    }
 }
 
 
-fn error_path(error: &str) -> path::PathBuf {
-    let mut path = PathBuf::from("error");
-    path.with_file_name(error);
-    path.set_extension("html");
-    path
-}
-
-async fn prepare_response(
-    status_code: StatusCode,
-    content_type: &str,
-    body: Body,
-) -> Response<Body> {
-    hyper::Response::builder()
-        .status(status_code)
-        .header(CONTENT_TYPE, content_type)
-        .body(body)
-        .unwrap()
-}
-
-async fn file_response_body(request_path: path::PathBuf) -> Result<Body, Error>
+async fn file_response_body(request_path: path::PathBuf) -> Result<Body, ServerError>
 {
     match File::open(&request_path).await {
         Ok(file) => {
-            let content_type = crate::resource::content_type(&request_path);
             let stream = FramedRead::new(file, BytesCodec::new());
             Ok(Body::wrap_stream(stream))
         }
-        Err(e) => {
-            println!("{:?}", e);
-            Err(e)
-        }
+        Err(e) => Err(ServerError::FileOpen(request_path))
     }
 }
 
 
-fn server_error_response() -> Response<Body> {
+fn failure_response() -> Response<Body> {
     Response::new(Body::from("Server Error"))
+}
+
+
+
+
+
+fn error_path() -> path::PathBuf {
+    let mut path = PathBuf::from("error");
+    path.with_file_name("404");
+    path.set_extension("html");
+    path
 }
